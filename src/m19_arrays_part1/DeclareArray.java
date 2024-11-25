8f40d4f9e8e87b04ffc11e68ae4f432e3b139ba6
velespitt <yusuf.asik123@gmail.com>
2024-11-25 23:25:50 +0300

    Array Intro

--- src/m19_arrays_part1/DeclareArray.java
+++ src/m19_arrays_part1/DeclareArray.java
@@ -14,15 +14,40 @@ public class DeclareArray {
 
         System.out.println(scores);//hashcode
 
+        System.out.println();
+
         System.out.println(Arrays.toString(scores));
+
+        System.out.println();
 
         System.out.println(scores[0]);
         System.out.println(scores[1]);
         System.out.println(scores[2]);
         System.out.println(scores[3]);
+
         //Throws OutOfBoundException System.out.println(scores[4]);
 
+        String[] names = {"Java", "Python","C#","Ruby","C++"};
+        int[] numbers = {10, 20, 30, 40, 70, 100, 200, 60, 80};
 
+        System.out.println();
+
+        System.out.println(Arrays.toString(names));
+
+        System.out.println();
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[4]);
+
+
+/*
+ Declaration and initialization can NOT be separated.
+int[] numbers2;
+numbers2 = {88, 99};//Compile error
+ */
 
 
     }
