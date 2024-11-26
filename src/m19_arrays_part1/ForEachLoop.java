1ee7060aec04dd8607e8dd57437eba1a909e5d24
velespitt <yusuf.asik123@gmail.com>
2024-11-26 22:08:58 +0300

    ForEachLoop Intro

---
+++ src/m19_arrays_part1/ForEachLoop.java
@@ -0,0 +1,14 @@
+package m19_arrays_part1;
+
+public class ForEachLoop {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {15, 30, 45, 60, 75, 90};
+
+        for (int each : numbers) {
+            System.out.println(each);//No index num and iteration fixed.
+        }
+
+    }
+}
